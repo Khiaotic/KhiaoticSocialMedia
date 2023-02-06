3693e3000ae9fe2e686a93b0233f3e5daaa33808
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-02-06 16:09:41 -0500

    readme

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,7 +1,7 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    getThoughts(req, res) {
+    getThoughts (req, res) {
         Thought.find ()
         .then((thoughts) => res.json(thoughts))
         .catch((err) =>res.status(500).json(err));
